4abbf3bd497fce8355acc48d1638ac13ce8aabb6
Ivan Company <ivangoblin@gmail.com>
2015-11-29 20:22:53 +0100

    First commit

---
+++ gulp/paths.js
@@ -0,0 +1,8 @@
+'use strict';
+
+import path from 'path';
+
+const root = path.dirname(__dirname);
+const paths = {
+	syles: `${root}/styles/`
+};
---
+++ gulp/tasks/styles.js
@@ -0,0 +1,25 @@
+'use strict';
+
+import gulp from 'gulp';
+import util from 'gulp-util';
+import sass from 'gulp-sass';
+import concat from 'gulp-concat';
+import filter from 'gulp-filter';
+import changed from 'gulp-changed';
+import sourcemaps from 'gulp-sourcemaps';
+import browserSync from 'browser-sync';
+import autoprefixer from 'gulp-autoprefixer';
+import path from '../paths';
+
+gulp.task('sass', () => {
+	return gulp.src(path.styles)
+		.pipe(changed(path.tmp.styles), {extension: '.scss'})
+		.pipe(sourcemaps.init())
+		.pipe(sass({style: 'compressed'}))
+		.pipe(autoprefixer('last 2 version'))
+		.pipe(concat('main.css'))
+		.pipe(sourcemaps.write('..maps'))
+		.pipe(gulp.dest(path.tmp.styles))
+		.pipe(filter('**/*.css'))
+		.pipe(browserSync.reload({stream: true}));
+});
